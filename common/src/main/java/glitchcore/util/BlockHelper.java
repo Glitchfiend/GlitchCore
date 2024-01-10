8424eaeb173355fa0b52ccaa3b976f98e57787f3
Adubbz <Adubbz@users.noreply.github.com>
2024-01-11 09:05:29 +1100

    Implemented BlockHelper

---
+++ common/src/main/java/glitchcore/util/BlockHelper.java
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ * Copyright 2024, the Glitchfiend Team.
+ * All rights reserved.
+ ******************************************************************************/
+package glitchcore.util;
+
+import com.google.common.collect.Maps;
+import net.minecraft.world.item.AxeItem;
+import net.minecraft.world.item.ShovelItem;
+import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.ComposterBlock;
+import net.minecraft.world.level.block.FireBlock;
+import net.minecraft.world.level.block.state.BlockState;
+
+public class BlockHelper
+{
+    public static void registerStrippable(Block log, Block stripped)
+    {
+        AxeItem.STRIPPABLES = Maps.newHashMap(AxeItem.STRIPPABLES);
+        AxeItem.STRIPPABLES.put(log, stripped);
+    }
+
+    public static void registerFlattenable(Block block, BlockState flattened)
+    {
+        ShovelItem.FLATTENABLES = Maps.newHashMap(ShovelItem.FLATTENABLES);
+        ShovelItem.FLATTENABLES.put(block, flattened);
+    }
+
+    public static void registerCompostable(float chance, ItemLike item)
+    {
+        ComposterBlock.COMPOSTABLES.put(item.asItem(), chance);
+    }
+
+    public static void registerFlammable(Block block, int encouragement, int flammability)
+    {
+        FireBlock fireblock = (FireBlock) Blocks.FIRE;
+        fireblock.setFlammable(block, encouragement, flammability);
+    }
+}

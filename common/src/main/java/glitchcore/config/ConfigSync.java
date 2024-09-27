15ce23f3e968bf1b6bea8ac19cd81c3c79fd6254
Marlester <77095872+marlester-dev@users.noreply.github.com>
2024-09-27 17:47:39 +0200

    add javadoc

--- common/src/main/java/glitchcore/config/ConfigSync.java
+++ common/src/main/java/glitchcore/config/ConfigSync.java
@@ -2,6 +2,13 @@ package glitchcore.config;
 
 public class ConfigSync {
 
+  /**
+   * Enables sync between server and client for your config.
+   * NOTE: This function works only on fabric, you need to implement it yourself
+   * if you want to use it on forge
+   *
+   * @param config your config.
+   */
   public static void register(Config config) {
     throw new UnsupportedOperationException();
   }

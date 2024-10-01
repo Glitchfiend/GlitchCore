51fe8250588546ebc3fb57a7318060f6b588b2d5
Marlester <77095872+marlester-dev@users.noreply.github.com>
2024-10-01 20:06:45 +0200

    make inited thread-safe

--- common/src/main/java/glitchcore/config/ConfigSync.java
+++ common/src/main/java/glitchcore/config/ConfigSync.java
@@ -6,26 +6,27 @@ import glitchcore.network.SyncConfigPacket;
 import glitchcore.util.Environment;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import net.minecraft.resources.ResourceLocation;
 
 public class ConfigSync {
   private static ResourceLocation configSyncChannel;
   public static PacketHandler packetHandler;
   public static final Map<String, Config> CONFIGS_BY_PATH = new HashMap<>();
-  private static boolean inited = false;
+  private static AtomicBoolean inited = new AtomicBoolean();
   /**
    * Enables sync between server and client for your config.
    *
    * @param config your config.
    */
   public static void register(Config config) {
-    if (!inited) {
+    if (inited.compareAndSet(false, true)) {
       configSyncChannel = new ResourceLocation(GlitchCore.MOD_ID, "config_sync");
       packetHandler = new PacketHandler(configSyncChannel);
       packetHandler.register(new ResourceLocation(GlitchCore.MOD_ID, "config_sync_packet"),
               new SyncConfigPacket());
       initFabric();
-      inited = true;
     }
     String relative = Environment.getConfigPath().relativize(config.getPath()).toString();
     CONFIGS_BY_PATH.put(relative, config);

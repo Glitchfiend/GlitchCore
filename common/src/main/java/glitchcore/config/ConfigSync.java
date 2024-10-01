e59fa3450515ca6af5de5f54ab1fb5cdd0a8d1c1
Marlester <77095872+marlester-dev@users.noreply.github.com>
2024-10-01 17:27:25 +0200

    ConfigSync on forge, yay

--- common/src/main/java/glitchcore/config/ConfigSync.java
+++ common/src/main/java/glitchcore/config/ConfigSync.java
@@ -1,34 +1,36 @@
 package glitchcore.config;
 
-import glitchcore.core.GlitchCore;
+import glitchcore.network.PacketHandler;
+import glitchcore.network.SyncConfigPacket;
 import glitchcore.util.Environment;
 import java.util.HashMap;
 import java.util.Map;
 import net.minecraft.resources.ResourceLocation;
 
 public class ConfigSync {
-  private static final ResourceLocation CONFIG_SYNC_CHANNEL = new ResourceLocation("glitchcorefabric", "config_sync");
-  private static final Map<String, Config> CONFIGS_BY_PATH = new HashMap<>();
+  private static ResourceLocation configSyncChannel;
+  public static PacketHandler packetHandler;
+  public static final Map<String, Config> CONFIGS_BY_PATH = new HashMap<>();
   private static boolean inited = false;
   /**
    * Enables sync between server and client for your config.
-   * NOTE: This function works only on fabric, you need to implement it yourself
-   * if you want to use it on forge
    *
    * @param config your config.
    */
   public static void register(Config config) {
     if (!inited) {
-      initSyncs();
+      configSyncChannel = new ResourceLocation("glitchcorefabric", "config_sync");
+      packetHandler = new PacketHandler(configSyncChannel);
+      packetHandler.register(configSyncChannel, new SyncConfigPacket());
+      initFabric();
       inited = true;
     }
     String relative = Environment.getConfigPath().relativize(config.getPath()).toString();
     CONFIGS_BY_PATH.put(relative, config);
   }
-  private static void initSyncs() {
-    throw new UnsupportedOperationException();
+  private static void initFabric() {
   }
-  private static void reload(String path, String toml) {
+  public static void reload(String path, String toml) {
     var config = CONFIGS_BY_PATH.get(path);
     config.parse(toml);
     config.load();

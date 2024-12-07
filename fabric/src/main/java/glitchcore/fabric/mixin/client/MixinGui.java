a6a709321faef8c354d870886d69b212008b58ad
Adubbz <Adubbz@users.noreply.github.com>
2024-12-07 11:50:43 +1100

    Fixes for Tough as Nails

--- fabric/src/main/java/glitchcore/fabric/mixin/client/MixinGui.java
+++ fabric/src/main/java/glitchcore/fabric/mixin/client/MixinGui.java
@@ -11,6 +11,7 @@ import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -20,6 +21,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(Gui.class)
 public abstract class MixinGui
 {
+    @Shadow protected abstract int getAirBubbleYLine(int $$0, int $$1);
+
     @Unique
     private DeltaTracker deltaTracker;
 
@@ -41,12 +44,12 @@ public abstract class MixinGui
         EventManager.fire(new RenderGuiEvent.Pre(RenderGuiEvent.Type.FOOD, (Gui)(Object)this, guiGraphics, this.deltaTracker, guiGraphics.guiWidth(), guiGraphics.guiHeight()));
     }
 
-    // TODO: Will require testing against Tough As Nails
-//    @ModifyVariable(method="renderAirBubbles", at=@At(value="INVOKE", target="net/minecraft/world/entity/player/Player.getMaxAirSupply()I"), ordinal = 4, require = 1)
-//    private int onBeginRenderAir(int rightTop, GuiGraphics guiGraphics, Player player, int i, int j, int k)
-//    {
-//        var event = new RenderGuiEvent.Pre(RenderGuiEvent.Type.AIR, (Gui)(Object)this, guiGraphics, this.deltaTracker, guiGraphics.guiWidth(), guiGraphics.guiHeight(), rightTop - 10);
-//        EventManager.fire(event);
-//        return event.getRowTop() + 10;
-//    }
+    @ModifyVariable(method="renderAirBubbles", at=@At(value="HEAD"), ordinal = 1, require = 1, argsOnly = true)
+    private int onBeginRenderAir(int rightTop, GuiGraphics guiGraphics, Player player, int vehicleHearts, int y, int x)
+    {
+        int yLine = this.getAirBubbleYLine(vehicleHearts, y);
+        var event = new RenderGuiEvent.Pre(RenderGuiEvent.Type.AIR, (Gui)(Object)this, guiGraphics, this.deltaTracker, guiGraphics.guiWidth(), guiGraphics.guiHeight(), yLine);
+        EventManager.fire(event);
+        return event.getRowTop() - 10;
+    }
 }
---
+++ forge/src/main/java/glitchcore/forge/mixin/client/MixinLevelRenderer.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * Copyright 2024, the Glitchfiend Team.
+ * All rights reserved.
+ ******************************************************************************/
+package glitchcore.forge.mixin.client;
+
+import com.mojang.blaze3d.resource.ResourceHandle;
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.PoseStack;
+import glitchcore.event.EventManager;
+import glitchcore.event.client.LevelRenderEvent;
+import net.minecraft.client.Camera;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.FogParameters;
+import net.minecraft.client.renderer.LevelRenderer;
+import net.minecraft.client.renderer.LightTexture;
+import net.minecraft.client.renderer.culling.Frustum;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(LevelRenderer.class)
+public class MixinLevelRenderer 
+{
+    @Shadow
+    private int ticks;
+
+    @Shadow @Final private Minecraft minecraft;
+
+    @Inject(method = "lambda$addParticlesPass$2", at=@At(value="INVOKE", target="Lnet/minecraft/client/particle/ParticleEngine;render(Lnet/minecraft/client/renderer/LightTexture;Lnet/minecraft/client/Camera;FLnet/minecraft/client/renderer/culling/Frustum;)V", shift= At.Shift.AFTER))
+    public void onAddParticles(FogParameters p_363695_, ResourceHandle resourcehandle1, ResourceHandle resourcehandle, LightTexture p_366434_, Camera camera, float p_365755_, Frustum frustum, CallbackInfo ci)
+    {
+        EventManager.fire(new LevelRenderEvent(LevelRenderEvent.Stage.AFTER_PARTICLES, (LevelRenderer)(Object)this, new PoseStack(), RenderSystem.getProjectionMatrix(), this.ticks, this.minecraft.getDeltaTracker(), camera, frustum));
+    }
+}

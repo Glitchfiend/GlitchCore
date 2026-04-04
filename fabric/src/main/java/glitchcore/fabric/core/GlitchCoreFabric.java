7378a3a30e9488ef08edf58c14d6cd038b2c2fdd
Adubbz <Adubbz@users.noreply.github.com>
2026-04-05 00:24:41 +1100

    Revert "Fabric: Ensure all registries are called"
    This reverts commit a2051c6ffb2b9d567b8a50999e120b79f7579a3f.

--- fabric/src/main/java/glitchcore/fabric/core/GlitchCoreFabric.java
+++ fabric/src/main/java/glitchcore/fabric/core/GlitchCoreFabric.java
@@ -20,13 +20,10 @@ import net.fabricmc.fabric.api.event.registry.FabricRegistry;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.data.registries.VanillaRegistries;
 import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
 
 import java.lang.reflect.Field;
-import java.util.LinkedHashSet;
-import java.util.Set;
 
 public class GlitchCoreFabric implements ModInitializer
 {
@@ -78,12 +75,8 @@ public class GlitchCoreFabric implements ModInitializer
 
     private static void postRegisterEvents()
     {
-        Set<Identifier> registries = new LinkedHashSet<>();
-        registries.addAll(VanillaRegistries.createLookup().listRegistryKeys().map(ResourceKey::identifier).toList());
-        registries.addAll(BuiltInRegistries.LOADERS.keySet());
-
         // We use LOADERS to ensure objects are registered at the correct time relative to each other
-        for (Identifier registryName : registries)
+        for (Identifier registryName : BuiltInRegistries.LOADERS.keySet())
         {
             ResourceKey<? extends Registry<?>> registryKey = ResourceKey.createRegistryKey(registryName);
             BuiltInRegistries.REGISTRY.get(registryName).ifPresent(registry -> {
